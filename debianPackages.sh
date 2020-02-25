bacf57544d620ae3899b1bf143f9a901fb76dffc
Luan F Barcelos <51036443+barcelosluan@users.noreply.github.com>
2020-02-24 22:08:08 -0300

    Update debianPackages.sh

--- debianPackages.sh
+++ debianPackages.sh
@@ -46,6 +46,7 @@ sudo apt install steam -y
 # Development
 # sudo snap install android-studio --classic
 # sudo snap install brackets --classic
+sudo apt install git
 # sudo snap install gitkraken
 # sudo snap install intellij-idea-community --classic
 # sudo apt install mariadb-server -y
