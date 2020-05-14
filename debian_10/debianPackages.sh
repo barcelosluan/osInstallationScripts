09080d907e2a8ace3f85fd92fac9f3d157652549
Luan F Barcelos <51036443+barcelosluan@users.noreply.github.com>
2020-05-14 15:10:36 -0300

    Add files via upload

---
+++ debian_10/debianPackages.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo ""
+echo "####################################"
+echo "#                                  #"
+echo "#    Debian fresh start install    #"
+echo "#       Made by: barcelosluan      #"
+echo "#        Updated 24-02-2020        #"
+echo "#         Enjoy the script         #"
+echo "#                                  #"
+echo "####################################"
+echo ""
+
+# Updating Sys
+sudo apt update -y
+sudo apt full-upgrade -y
+
+# ADD 'contrib non-free' Repository
+sudo nano /etc/apt/sources.list
+sudo apt update -y
+
+# Enable multi-arch
+sudo dpkg --add-architecture i386
+sudo apt update -y
+
+# Nvidia & Vulkan
+sudo apt install nvidia-driver -y
+sudo apt install nvidia-driver-libs-i386 -y
+sudo apt install nvidia-vulkan-icd -y
+sudo apt install nvidia-vulkan-icd:i386 -y
+
+# Snap Install
+sudo apt install snapd -y
+
+# Browsers
+sudo apt install epiphany-browser -y
+sudo snap install firefox
+
+# Utils
+sudo apt install alacarte -y
+# sudo snap install gimp
+# sudo snap install inkscape
+sudo apt install ttf-mscorefonts-installer -y
+sudo snap install vlc
+sudo snap install spotify
+sudo apt install steam -y
+
+# Development
+# sudo snap install android-studio --classic
+# sudo snap install brackets --classic
+sudo apt install git
+# sudo snap install gitkraken
+# sudo snap install intellij-idea-community --classic
+# sudo apt install mariadb-server -y
+# sudo dnf install -y nginx
+sudo snap install code --classic
+
+# Remove unecessary packages
+sudo apt auto-remove -y
---
+++ windows_10/windows_README.md
@@ -0,0 +1,8 @@
+### Windows
+	
+	chocolateyPackages.ps1
+
+1. Leia todo o documento e os scripts (se necessário atualize!) :bookmark_tabs:
+2. Abra o prompt de comando `cmd` no modo administrador :warning:
+3. Execute o arquivo `chocolateyPackages.ps1` :chocolate_bar:
+4. __Divirta-se :smiley:__
