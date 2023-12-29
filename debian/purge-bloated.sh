ea89e92697391eeed9970c4dfaba5a6ae94a3f77
Luan Oliveira Peralta <luan.barcelos@passeidireto.com>
2023-12-29 00:25:32 -0300

    Create purge-bloated.sh

---
+++ debian/purge-bloated.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sudo apt purge -y \
+  gnome-2048 \
+  gnome-calendar \
+  cheese \
+  five-or-more \
+  gnome-contacts \
+  evolution \
+  gnome-color-manager \
+  gnome-nibbles \
+  gnome-sound-recorder \
+  hitori \
+  gnome-klotski \
+  libreoffice \
+  gnome-logs \
+  lightsoff \
+  gnome-mahjongg \
+  gnome-maps \
+  gnome-weather \
+  gnome-mines \
+  gnome-music \
+  aisleriot \
+  quadrapassel \
+  four-in-a-row \
+  gnome-clocks \
+  iagno \
+  rhythmbox* \
+  gnome-robots \
+  seahorse \
+  shotwell* \
+  gnome-sudoku \
+  swell-foop \
+  tali \
+  gnome-taquin \
+  synaptic \
+  im-config \
+  zutty \
+  gnome-tetravex \
+  network-manager-gnome \
+  totem* \
+  gnome-chess \
+  yelp
+
+sudo apt autopurge -y
